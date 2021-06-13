75d5be7b6818599d54f00d5a087b7355b6ca8199
DamianColazo <31802737+DamianColazo@users.noreply.github.com>
2021-06-13 19:41:43 -0300

    Modificacion en BD

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: 'variables.env'})
 const db = new Sequelize(
     process.env.BD_NOMBRE, 
     process.env.BD_USER, 
-    process.env.BD_PASS, 
+    '', 
     {
         host: process.env.BD_HOST,
         dialect: 'mysql',
